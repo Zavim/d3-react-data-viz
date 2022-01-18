6f3bcb92f591b3e162ec8aae761d1bbc9782cda2
Zavim <Zark471@gmail.com>
2022-01-18 17:01:57 -0500

    f: update fetchText function

--- src/useData.js
+++ src/useData.js
@@ -2,3 +2,29 @@ import React, { useState, useEffect } from "react";
 import { csv } from 'd3';
 
 const csvUrl = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv';
+
+export const useData = () => {
+	const [data, setData] = useState(null);
+
+	const fetchText = async (file) => {
+		return await file.text();
+	};
+
+	const handleChange = (event) => {
+		let file = event.target.files[0];
+		fetchText(file).then(text => {
+			const data = csvParse(text);
+			setData(data);
+		}
+		);
+	};
+
+	// useEffect(() => {
+	// 	const row = d => {
+
+	// 	}
+	// 	csv(csvUrl, row).then(data => {
+	// 		setData(data);
+	// 	})
+	// },[])
+};
